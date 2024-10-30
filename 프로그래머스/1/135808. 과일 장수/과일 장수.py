37a33ad71783ca1518930b5d4ac6fa2ec0278ea4
soojung <141158150+soojxng@users.noreply.github.com>
2024-10-30 23:34:35 +0900

    [level 1] Title: 과일 장수, Time: 123.86 ms, Memory: 21.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,9 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort()
+    score.reverse()
+    
+    for i in range(m - 1, len(score), m):
+        answer = answer + (score[i] * m)
+
+    return answer

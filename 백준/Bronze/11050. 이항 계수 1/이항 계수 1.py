bd90c5aafeeb11a3e273b0de744249497a473723
soojung <141158150+soojxng@users.noreply.github.com>
2025-03-16 20:17:07 +0900

    [Bronze I] Title: 이항 계수 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-K = K if N - K > K else N - K
 n, k = 1, 1
 for i in range(1, K+1):
-    n *= (N + 1 - i)
     k *= i
+for j in range(N, N-K, -1):
+    n *= j
      
 print(n//k)

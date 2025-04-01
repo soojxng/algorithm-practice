4710bd6c861e29bca835fe97a4b5819ba111a15d
soojung <141158150+soojxng@users.noreply.github.com>
2025-04-02 01:47:23 +0900

    [Silver V] Title: 집합, Time: 3008 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,21 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-S = set()
-allS = set(range(1, 21))
+emptyS = [0 for _ in range(21)]
+allS = [1 for _ in range(21)]
+S = emptyS[:]
 
 for _ in range(int(input())):
     tmp = input().split()
     if tmp[0] == 'add':
-        S.add(int(tmp[1]))
+        S[int(tmp[1])] = 1
     elif tmp[0] == 'remove':
-        S.discard(int(tmp[1])) 
+        S[int(tmp[1])] = 0
     elif tmp[0] == 'check':
-        print(1 if int(tmp[1]) in S else 0)
+        print(S[int(tmp[1])])
     elif tmp[0] == 'toggle':
-        S.discard(int(tmp[1])) if int(tmp[1]) in S else S.add(int(tmp[1]))
+        S[int(tmp[1])] = 0 if S[int(tmp[1])] else 1
     elif tmp[0] == 'all':
-        S = allS.copy()
+        S = allS[:]
     elif tmp[0] == 'empty':
-        S.clear()
+        S = emptyS[:]
         

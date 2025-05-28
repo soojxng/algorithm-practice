0195f81c938cb6df57606831568d6d8711bedfc6
soojung <141158150+soojxng@users.noreply.github.com>
2025-05-28 23:22:47 +0900

    [Silver V] Title: 초코바 만들기, Time: 252 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/33520. 초코바 만들기/초코바 만들기.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+w, h = 0, 0
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    if a > b: a, b = b, a
+    w = max(w, a)
+    h = max(h, b)
+print(w*h)

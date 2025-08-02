eecf5b56fc897aea45dfa894f3dfb30c5fc4837d
soojung <141158150+soojxng@users.noreply.github.com>
2025-08-02 13:44:58 +0900

    [level 2] Title: 노선별 평균 역 사이 거리 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284531. 노선별 평균 역 사이 거리 조회하기/노선별 평균 역 사이 거리 조회하기.sql
@@ -0,0 +1,4 @@
+SELECT route, CONCAT(ROUND(SUM(d_between_dist), 1), 'km') AS total_distance, CONCAT(ROUND(AVG(d_between_dist), 2), 'km') AS average_distance
+FROM subway_distance
+GROUP BY route
+ORDER BY ROUND(SUM(d_between_dist), 1) DESC;

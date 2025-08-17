bea4d0ee93d6cacc981ec3a10f5e15846a273186
soojung <141158150+soojxng@users.noreply.github.com>
2025-08-17 20:10:32 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3736 ms, Memory: 199932 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+def find(n):
+    if parents[n] != n:
+        parents[n] = find(parents[n])
+    return parents[n]
+
+def union(a, b):
+    parents[find(a)] = find(b)
+
+v, e = map(int, input().split())
+edges = []
+parents = [i for i in range(v+1)]
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+edges.sort()
+
+ans = 0
+tmp = 0
+for c, a, b in edges:
+    if find(a) != find(b):
+        union(a, b)
+        ans += c
+        tmp = c
+print(ans-tmp)

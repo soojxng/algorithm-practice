4dc78c9413430e6d0ecd35bdcbe29112438d3b86
soojung <141158150+soojxng@users.noreply.github.com>
2025-10-04 21:15:08 +0900

    [Gold V] Title: 두 부분 문자열, Time: 480 ms, Memory: 40220 KB -BaekjoonHub

---
+++ 백준/Gold/11056. 두 부분 문자열/두 부분 문자열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+A = input().strip()
+B = input().strip()
+dp = [[0]*(len(B)+1) for _ in range(len(A)+1)]
+
+for i in range(1, len(A)+1):
+    for j in range(1, len(B)+1):
+        if A[i-1] == B[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(len(A) + len(B) - dp[-1][-1])

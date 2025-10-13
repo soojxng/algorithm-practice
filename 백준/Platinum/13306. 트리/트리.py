9ba1ec4529f95be422eb004eeda2ef4462d26d79
soojung <141158150+soojxng@users.noreply.github.com>
2025-10-13 17:20:14 +0900

    [Platinum IV] Title: 트리, Time: 468 ms, Memory: 144600 KB, Score: 43 point -BaekjoonHub

--- 백준/Platinum/13306. 트리/트리.py
+++ 백준/Platinum/13306. 트리/트리.py
@@ -6,16 +6,29 @@ def find(v):
         v = parents[v]
     return v
 
+def union(a, b):
+    pa = find(a)
+    pb = find(b)
+    if pa != pb:
+        parents[pb] = pa
+
 n, q = map(int, input().split())
 
 parents = [i for i in range(n+1)]
+par = [i for i in range(n+1)]
 for i in range(2, n+1):
     a = int(input())
-    parents[i] = a
+    par[i] = a
 
-for _ in range(n - 1 + q):
-    tmp = list(map(int, input().split()))
-    if not tmp[0]:
-        parents[tmp[1]] = tmp[1]
+tmp = [list(map(int, input().split())) for _ in range(n-1+q)]
+tmp.reverse()
+
+ans = []
+for t in tmp:
+    if not t[0]:
+        union(t[1], par[t[1]])
     else:
-        print('YES' if find(tmp[1]) == find(tmp[2]) else 'NO')
+        ans.append(find(t[1]) == find(t[2]))
+
+for a in reversed(ans):
+    print('YES' if a else 'NO')
